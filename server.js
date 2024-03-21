a054c15573edb4cb725a556add7235a8761e19c8
lucas <zaratinlucas@gmail.com>
2024-03-21 16:04:04 -0300

    API Node com Postgres

---
+++ server.js
@@ -0,0 +1,53 @@
+//import { DatabaseMomory } from './database-memory.js'
+import {fastify} from 'fastify'
+import { DatabasePostgress } from './databese-postgres.js'
+
+const server = fastify()
+
+//const database = new DatabaseMomory()
+const database = new DatabasePostgress()
+
+server.post('/videos', async(request, reply) => {
+    const {title, description, duration} = request.body
+    
+    await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async(request) =>{
+    const search = request.query.search
+    console.log(search)
+
+    const videos =  await database.lsit(search)
+
+    return videos
+})
+
+server.put('/videos/:id', async(request, reply) =>{
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    await database.updated(videoId,{
+        title,
+        description,
+        duration,
+    })
+    
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', async(request, reply) =>{
+    const videoId = request.params.id
+
+    await database.delete(videoId)
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: process.env.PORT ?? 3333,
+})
